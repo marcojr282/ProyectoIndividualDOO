6c5f62e48c57c45d8c001ac562bdf1b69f0c7f07
Marcojr282 <marco.zapata282@gmail.com>
2024-04-26 23:53:07 -0500

    26/04/2024

--- PCH/src/main/java/co/edu/uco/dto/DepartamentoDTO.java
+++ PCH/src/main/java/co/edu/uco/dto/DepartamentoDTO.java
@@ -7,34 +7,40 @@ public final class DepartamentoDTO {
 	private String nombre;
 	private PaisDTO pais;
 	
-
 	public DepartamentoDTO() {
 		super();
-
-	}
-	public DepartamentoDTO(final UUID id, final String nombre,final PaisDTO pais) {
-		setId(id);
-		setNombre(nombre);
-		setPais(pais);
-	}
-	private UUID getId() {
-		return id;
-	}
-	private void setId(UUID id) {
-		this.id = id;
-	}
-	private final String getNombre() {
-		return nombre;
-	}
-	private  final void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	private  PaisDTO getPais() {
-		return pais;
-	}
-	private final void setPais(PaisDTO pais) {
-		this.pais = pais;
 	}
 	
+	public DepartamentoDTO(final UUID id, final String nombre, final PaisDTO pais) {
+		super();
+		setId (id);
+		setNombre (nombre);
+		setPais  (pais);
+	}
+	
+	public static final DepartamentoDTO build() {
+		return new DepartamentoDTO();
+	}
+	public final UUID getId() {
+		return id;
+	}
+	public final DepartamentoDTO setId(final UUID id) {
+		this.id = id;
+		return this;
+	}
+	public final String getNombre() {
+		return nombre;
+	}
+	public final  DepartamentoDTO setNombre(final String nombre) {
+		this.nombre = nombre;
+		return this;
+	}
+	public final PaisDTO getPais() {
+		return pais;
+	}
+	public final  DepartamentoDTO setPais(final PaisDTO pais) {
+		this.pais = pais;
+		return this;
+	}
 
 }
